0dc37ab43f0f0ed256b9c4375f8a61a918f38939
ventu-emme <ventu.emme@gmail.com>
2014-01-22 00:43:36 +0100

    rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zoo::Application.config.secret_key_base = 'e3606a09ffc2c0af49119788918f7da5224ad3f963d48f3fd1d293d631335b0ff4163729e6953264e2aa40e2ccfc16ea1c4c8a6d0fc849118c719ef636df6b2d'
