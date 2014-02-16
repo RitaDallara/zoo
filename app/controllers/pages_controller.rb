e2f676438751be0a30c8d56c27546b39e119fe41
ventu-emme <ventu.emme@gmail.com>
2014-02-16 18:10:37 +0100

    user score is sent to server at the end of a game

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,25 @@ class PagesController < ApplicationController
   end
 
   def game
-    @num_quiz= params[:num]
   end
 
   def about
   end
+
+  def ranking
+    @username= current_user.username
+
+    @current_user = current_user
+    @current_user.update_max_score(params[:diff],params[:score])
+
+    #current_user.update_max_score(params[:diff],params[:score])
+
+    @current_user.save!
+    #current_user.save!
+
+    @score = current_user.max_easy
+
+
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,30 @@ class User < ActiveRecord::Base
       add_role(:user)
     end
 
+    def update_max_score(diff,score)
+      #ref_score = self.max_easy if diff == "easy"
+      #ref_score = self.max_medium if diff == "medium"
+      #ref_score = self.max_hard if diff == "hard"
+      #ref_score = "max_#{diff}"
+      update_attributes( :"max_#{diff}" =>  [send("max_#{diff}"), score.to_i].max  )
+      #if diff == "easy"
+      #  if score.to_i > self.max_easy
+      #    self.max_easy= score.to_i
+      #  end
+	
+      #elsif diff == "medium"
+      #  if score.to_i > self.max_medium
+      #    self.max_medium= score.to_i
+      #  end
+	
+      #elsif diff == "hard"
+	#if score.to_i > self.max_hard
+	#  self.max_hard= score.to_i
+	#end
+      #end
+      
+    end
+
 	rolify
 
 	devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
